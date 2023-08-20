1956562c7cd62991b802cc1db3c7d4bf5e882038
Urala Disawage Ranithu Kenula Francis <133321142+kenulafrancis@users.noreply.github.com>
2023-08-20 19:12:09 +0530

    Update details.js

--- HTMl assingment draft/2023/assingment 2/js-as3/details.js
+++ HTMl assingment draft/2023/assingment 2/js-as3/details.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("details-section");
   const continueBtn = document.getElementById("continueBtn");
+  const nameRegex = /^[A-Za-z\s]+$/; // Regular expression to validate names
+  const phoneRegex = /^\d+$/; // Regular expression to validate phone numbers
+
+  const fullNameError = document.getElementById("fullNameError");
+  const mobileNumberError = document.getElementById("mobileNumberError");
+  const emailError = document.getElementById("emailError");
+  const confirmEmailError = document.getElementById("confirmEmailError");
 
   // Initialize intl-tel-input plugin on the mobileNumber input
   const mobileNumberInput = document.getElementById("mobileNumber");
@@ -17,43 +24,20 @@ document.addEventListener("DOMContentLoaded", function () {
     form.confirmEmail.value = userData.confirmEmail;
     form.gender.value = userData.gender;
 
-    // Load summary table values from Ticket page localStorage
-    const summaryDate = localStorage.getItem("summaryDate");
-    const summaryTime = localStorage.getItem("summaryTime");
-    const summaryDuration = localStorage.getItem("summaryDuration");
-    const summaryTickets = localStorage.getItem("summaryTickets");
-    const summaryTotal = localStorage.getItem("summaryTotal");
-
-    // Populate the summary table with Ticket page data
-    document.getElementById("summaryDate").textContent = summaryDate || "-";
-    document.getElementById("summaryTime").textContent = summaryTime || "-";
-    
-    // Calculate Normal and Peak durations based on Ticket page data
-    const selectedTimeSlots = (summaryDuration || "").split(",");
-    let normalDuration = 0;
-    let peakDuration = 0;
-    for (const slot of selectedTimeSlots) {
-      if (slot.includes("Normal")) {
-        normalDuration++;
-      } else if (slot.includes("Peak")) {
-        peakDuration++;
-      }
-    }
-    
-    const durationText = `Normal: ${normalDuration}, Peak: ${peakDuration}`;
-    document.getElementById("summaryDuration").textContent = durationText || "-";
-
-    document.getElementById("summaryTickets").innerHTML = summaryTickets || "-";
-    document.getElementById("summaryTotal").textContent = summaryTotal || "-";
+    // ... Load summary table values and populate the summary table ...
   }
 
   form.addEventListener("input", () => {
-    const isFormValid = form.checkValidity();
+    const isFormValid = form.checkValidity() && validateInputs();
     continueBtn.disabled = !isFormValid;
   });
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    if (!validateInputs()) {
+      return; // Don't proceed if inputs are invalid
+    }
 
     // Get the selected country code and phone number
     const selectedCountryCode = iti.getSelectedCountryData().dialCode;
@@ -73,4 +57,43 @@ document.addEventListener("DOMContentLoaded", function () {
     // Redirect to Payment page
     window.location.href = "payment.html";
   });
+
+  // Function to validate name, phone number, and email inputs
+  function validateInputs() {
+    const fullNameInput = form.fullName;
+    const mobileNumberInput = form.mobileNumber;
+    const emailInput = form.email;
+    const confirmEmailInput = form.confirmEmail;
+  
+    const fullName = fullNameInput.value.trim();
+    const mobileNumber = mobileNumberInput.value.trim();
+    const email = emailInput.value.trim();
+    const confirmEmail = confirmEmailInput.value.trim();
+  
+    const isNameValid = nameRegex.test(fullName);
+    const isPhoneValid = phoneRegex.test(mobileNumber);
+    const isEmailValid = isValidEmail(email);
+    const areEmailsMatching = email === confirmEmail;
+  
+    fullNameError.textContent = isNameValid ? "" : "Please enter a valid name.";
+    mobileNumberError.textContent = isPhoneValid ? "" : "Please enter a valid phone number.";
+    emailError.textContent = isEmailValid ? "" : "Please enter a valid email address.";
+    confirmEmailError.textContent = areEmailsMatching ? "" : "Email addresses do not match.";
+  
+    // Set error message text color to red
+    const errorTextColor = "#FF0000"; // Red color
+    fullNameError.style.color = isNameValid ? "" : errorTextColor;
+    mobileNumberError.style.color = isPhoneValid ? "" : errorTextColor;
+    emailError.style.color = isEmailValid ? "" : errorTextColor;
+    confirmEmailError.style.color = areEmailsMatching ? "" : errorTextColor;
+  
+    return isNameValid && isPhoneValid && isEmailValid && areEmailsMatching;
+  }  
+
+  // Function to validate email format
+  function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
 });
+
